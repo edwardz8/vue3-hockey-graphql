597a1e42c355f5464f2285ff4aa37247eac9c196
edwardz8 <tolklorist@gmail.com>
2020-12-24 18:26:15 -0500

    graphql ID working

--- src/components/PlayerDetails.vue
+++ src/components/PlayerDetails.vue
@@ -1,14 +1,40 @@
 <template>
-  <div>
-    <h1>{{ pitcher.player }}</h1>
-    <h1>{{ pitcher.wins }}</h1>
-  </div>
+  <div class="container mx-auto py-2">
+      <div class="border m-6 rounded-lg bg-white mx-auto max-w-sm shadow-lg rounded-lg overflow-hidden">
+        <div class="clearfix sm:flex sm:items-center px-12 py-4 hover:bg-gray-300">
+          <p
+            class="logo xl:float-left lg:float-left xl:mr-4 lg:mr-4 md:mr-4 md:my-2 xl:my-2 lg:my-2"
+          >
+            <i :class="matchTeamLogo(pitcher.team)"></i>
+          </p>
+          <div class="text-center sm:text-left sm:flex-grow">
+            <div class="mb-4">
+              <p class="font-sans text-xl leading-tight mb-2">{{ pitcher.player }}</p>
+              <p class="font-sans text-sm leading-tight text-grey-dark mb-2">{{ pitcher.team }}</p>
+              <p
+                class="font-sans text-sm leading-tight"
+              >Wins: {{ pitcher.wins }} - Innings: {{ pitcher.innings_pitched }}</p>
+            </div>
+            <div class="sm:flex sm:items-center flex-wrap">
+              <button class="text-xs font-semibold rounded-full px-4 py-1 mx-3 leading-normal bg-white border border-blue text-blue hover:text-black"
+              >Track</button>
+              <router-link to="/">
+                <button
+                  class="text-xs font-semibold rounded-full px-4 py-1 leading-normal bg-white border border-purple text-purple hover:text-black"
+                >All Players</button>
+              </router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 </template>
 
 <script>
-import { computed, watchEffect } from "vue";
+import { computed, watch } from "vue";
 import { useQuery, useResult } from "@vue/apollo-composable";
 import gql from "graphql-tag";
+import methods from "../methods";
 
 export default {
   name: "PlayerDetails",
@@ -18,27 +44,32 @@ export default {
   setup(props) {
     const { result } = useQuery(
       gql`
-        query pitcher ($playerid: ID!) {
+        query pitcher ($playerid: Int!) {
           pitcher (playerid: $playerid) {
             player
             playerid
             team
+            innings_pitched
             wins
           }
         }
-      `, props
+      `,
+      props
     );
 
-    const pitcher = useResult(result, (data) => data.pitcher);
+    const pitcher = useResult(result, null, (data) => data.pitcher);
 
-    watchEffect(() => {
+    watch(() => {
       console.log(result.value); // undefined
     });
 
     return {
       result,
-      pitcher
+      pitcher,
     };
+  },
+  methods: {
+    ...methods,
   },
 };
 </script>
